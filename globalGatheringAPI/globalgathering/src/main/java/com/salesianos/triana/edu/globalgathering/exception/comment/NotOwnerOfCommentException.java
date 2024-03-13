b19a672654c7951f95b31f32097c7d4981a0a00f
Cristian Pulido Cabello <114216690+cpcx04@users.noreply.github.com>
2024-03-13 14:46:10 +0100

    Delete globalGatheringAPI directory

--- globalGatheringAPI/globalgathering/src/main/java/com/salesianos/triana/edu/globalgathering/exception/comment/NotOwnerOfCommentException.java
+++
@@ -1,6 +0,0 @@
-package com.salesianos.triana.edu.globalgathering.exception.comment;
-
-public class NotOwnerOfCommentException extends RuntimeException{
-
-    public NotOwnerOfCommentException(){super("You cant delete this comment");}
-}
