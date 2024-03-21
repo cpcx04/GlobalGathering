cff871e70bf704cdb1eacdd4867121f416986480
cpcx04 <pulidocabellochristian@gmail.com>
2024-03-21 18:12:23 +0100

    authGuard added

---
+++ globalGatheringWeb/globalGatheringWeb/src/app/services/auth.guard.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
+
+ 
+
+@Injectable({
+
+  providedIn: 'root'
+
+})
+
+export class AuthGuard implements CanActivate {
+
+ 
+
+  constructor (public _authService: AuthService,
+
+               public router: Router) {}
+
+  canActivate(
+
+    next: ActivatedRouteSnapshot,
+
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+
+      if (!this._authService.isLoggedIn()) {
+
+        this.router.navigate(['login']);
+
+        return false;
+
+      }
+
+      return true;
+
+  }
+
+}
--- globalGatheringWeb/globalGatheringWeb/src/app/services/auth.service.ts
+++ globalGatheringWeb/globalGatheringWeb/src/app/services/auth.service.ts
@@ -1,23 +1,45 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 import { enviroment } from '../enviroments/enviroment';
 import { LoginResponse } from '../models/login-response.interface';
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-export class AuthService{
-    constructor(private http: HttpClient){}
-    
-    login(username: string,password:string): Observable<LoginResponse>{
-        return this.http.post<LoginResponse>(`${enviroment.apiBaseUrl}/auth/login`,{
+export class AuthService {
+    private apiUrl = `${enviroment.apiBaseUrl}/auth`;
+    private authTokenKey = 'authToken';
+
+    constructor(private http: HttpClient, private router: Router) { }
+
+    login(username: string, password: string): Observable<LoginResponse> {
+        return this.http.post<LoginResponse>(`${this.apiUrl}/login`, {
             username: username,
             password: password,
-        },{
+        }, {
             headers: {
-            'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             },
-        })
+        }).pipe(
+            tap(response => {
+                localStorage.setItem(this.authTokenKey, response.token);
+                this.router.navigate(['/home']); 
+            }),
+            catchError(error => {
+                throw error;
+            })
+        );
+    }
+
+    logout(): void {
+        localStorage.removeItem(this.authTokenKey);
+        this.router.navigate(['/login']);
+    }
+
+    isLoggedIn() {
+        return !!localStorage.getItem(this.authTokenKey);
     }
 }
