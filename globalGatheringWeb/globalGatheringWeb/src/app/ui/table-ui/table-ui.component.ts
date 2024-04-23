9f1d8dcf94bbbc047142ab912ffda595fafc2189
Cristian <pulidocabellochristian@gmail.com>
2024-04-23 12:41:31 +0200

    Trying to resolve some bugs

--- globalGatheringWeb/globalGatheringWeb/src/app/ui/table-ui/table-ui.component.ts
+++ globalGatheringWeb/globalGatheringWeb/src/app/ui/table-ui/table-ui.component.ts
@@ -2,21 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { ClienteService } from '../../services/clientes/cliente.service';
 import { ClienteResponse } from '../../models/clients.interface';
 
-
 @Component({
   selector: 'app-table-ui',
   templateUrl: './table-ui.component.html',
   styleUrls: ['./table-ui.component.css']
 })
 export class TableUiComponent implements OnInit {
-  
-deleteUser(_t14: ClienteResponse) {
-throw new Error('Method not implemented.');
-}
-editUser(_t14: ClienteResponse) {
-throw new Error('Method not implemented.');
-}
+  searchQuery: string = '';
   clientes: ClienteResponse[] = [];
+  selectedCliente: ClienteResponse | null = null;
+  isDeleteModalOpen: boolean = false;
+  isEditModalOpen: boolean = false;
 
   constructor(private clienteService: ClienteService) { }
 
@@ -24,5 +20,54 @@ throw new Error('Method not implemented.');
     this.clienteService.getAllClients().subscribe(response => {
       this.clientes = response;
     });
+  }
+
+  deleteUserConfirmation(cliente: ClienteResponse) {
+    this.selectedCliente = cliente;
+    this.isDeleteModalOpen = true;
+  }
+
+  cancelEditUser(){
+    this.isEditModalOpen = false;
+    this.selectedCliente = null;
+  }
+
+  cancelDeleteUser() {
+    this.isDeleteModalOpen = false;
+    this.selectedCliente = null;
+  }
+
+  editUser(cliente: ClienteResponse) {
+    this.selectedCliente = cliente;
+    this.isEditModalOpen = true;
+  }
+
+  confirmDeleteUser() {
+    if (this.selectedCliente) {
+        console.log('Usuario eliminado:', this.selectedCliente);
+        const index = this.clientes.indexOf(this.selectedCliente);
+        if (index !== -1) {
+            this.clientes.splice(index, 1);
+        }
+    }
+    this.isDeleteModalOpen = false;
+    this.selectedCliente = null;
+}
+
+  saveEditedUser() {
+      if (this.selectedCliente) {
+          console.log('Usuario editado:', this.selectedCliente);
+      }
+      this.isEditModalOpen = false;
+      this.selectedCliente = null;
+  }
+
+
+  get filteredClientes(): ClienteResponse[] {
+    return this.clientes.filter(cliente =>
+      cliente.username.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+      cliente.email.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+      cliente.nombre.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
   }
 }
